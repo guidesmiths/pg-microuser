d75d6dec2001b1a5b50f7ba8912aef690cbf6334
Stephen Cresswell <git@stephen-cresswell.net>
2017-03-01 13:05:19 +0000

    Working around some AWS issues

--- run.sh
+++ run.sh
@@ -42,13 +42,14 @@ if [ "$1" == "create-schema" ]; then
     OWNER=$2
     READ_ONLY_USER=$3
     ADMIN_USER=$4
+    SUPER_USER=$PGUSER
 
     if [ -z "$OWNER" ] || [ -z "$READ_ONLY_USER" ] || [ -z "$ADMIN_USER" ]; then
         echo "Usage: run.sh create-schema <owner> <read only user> <admin user>"
         exit 1
     fi
 
-    SQL=$(sed "s/__OWNER__/$OWNER/g" create-schema.sql | sed "s/__READ_ONLY_USER__/$READ_ONLY_USER/g" | sed "s/__ADMIN_USER__/$ADMIN_USER/g")
+    SQL=$(sed "s/__SUPER__/$SUPER_USER/g" create-schema.sql | sed "s/__OWNER__/$OWNER/g" | sed "s/__READ_ONLY_USER__/$READ_ONLY_USER/g" | sed "s/__ADMIN_USER__/$ADMIN_USER/g")
     echo $SQL
     psql << EOF
     $SQL
